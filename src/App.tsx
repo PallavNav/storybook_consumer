6a2330565025ef7a4be2d7092c72b9610265eb35
npallav <nav.pallav@mercedes-benz.com>
2023-09-08 13:10:43 +0530

    small changes.

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Consumer App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button label='Hello Nav! I am from Producer App!' onClick={handleButtonClick}></Button>
+        <Button label='Hello Nav! I am the first Producer App!' onClick={handleButtonClick}></Button>
       </header>
     </div>
   );
